8e69da17b4eedee36dc1a0199f82a23eb5f778b4
Tom Gates <thomas.gates@shopify.com>
2021-07-22 12:36:03 -0400

    install solargraph for vs code

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,6 @@ ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 sudo wget -O /usr/local/bin/rsub https://raw.github.com/aurora/rmate/master/rmate
 sudo chmod a+x /usr/local/bin/rsub
 sudo mv /usr/local/bin/rmate /usr/local/bin/subl
+
+# Installs Ruby language server
+sudo gem install solargraph
